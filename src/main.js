6a962494d068ae0df791608d7c8d806e5db095c3
huanganfree <huangan1206@163.com>
2024-04-08 08:35:00 +0800

    feat: 添加store全局状态管理

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
+import store from './store/index'
+
 import './assets/common.less'
 import './utils/rem';
 
 const app = createApp(App)
 
-app.use(router).use(Antd).mount('#app')
+app.use(router).use(store).use(Antd).mount('#app')
