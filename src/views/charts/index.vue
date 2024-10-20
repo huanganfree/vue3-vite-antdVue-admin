68ea8e2b7ef8e883aedac68b0b79ef0634da86d2
huanganfree <huangan1206@163.com>
2024-10-21 07:39:12 +0800

    feat: 完成折线图

--- src/views/charts/index.vue
+++ src/views/charts/index.vue
@@ -2,10 +2,12 @@
   <div class="charts">
     <Map />
     <Bar />
+    <Line />
   </div>
 </template>
 
 <script setup>
 import Map from './map.vue'
 import Bar from './bar.vue'
+import Line from './line.vue'
 </script>
---
+++ src/views/charts/line.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="wrapper" ref="containerRef">
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as echarts from "echarts";
+
+
+const containerRef = ref(null);
+onMounted(() => {
+    initData()
+});
+
+
+function initData() {
+    var myChart = echarts.init(containerRef.value);
+    let option = {
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                areaStyle: {},
+                lineStyle: {
+                    color: '#2EF1FF'
+                },
+                symbol: "none",
+                smooth: true,
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0, color: '#2E94FF' // 0% 处的颜色
+                }, {
+                    offset: 0.4, color: '#2E94FF' // 100% 处的颜色
+                }, {
+                    offset: 1, color: '#e4f2ff' // 100% 处的颜色
+                }]
+                )
+            }
+        ]
+    };
+    myChart.setOption(option);
+}
+
+</script>
+<style lang="less">
+.tooltip-wrapper {
+    padding: 10px;
+    border-radius: 6.833px;
+    border: 0.854px solid rgba(255, 255, 255, 0.40);
+    background: linear-gradient(180deg, rgba(88, 169, 255, 0.47) 0%, rgba(63, 83, 248, 0.47) 100%);
+    box-shadow: 0px 3.417px 25.625px 0px rgba(5, 21, 71, 0.25);
+    backdrop-filter: blur(12.812499046325684px);
+
+    .flex {
+        display: flex;
+        align-items: center;
+    }
+
+    .marginRight {
+        margin-right: 8px;
+    }
+
+    .city-total {
+        margin-bottom: 7px;
+        font-size: 17.083px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        color: #fff;
+
+
+
+        .city-name {
+            justify-content: space-between;
+
+        }
+
+    }
+
+    .type {
+
+        .one-name,
+        .two-name {
+            color: rgba(255, 255, 255, 0.70);
+            font-size: 11.958px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: normal;
+        }
+
+        .type-value {
+            color: #fff;
+            font-size: 11.958px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: normal;
+        }
+    }
+
+}
+</style>
+<style lang="less" scoped>
+.wrapper {
+    width: 398px;
+    height: 506px;
+    border-radius: 18px;
+    border: 0.854px solid #4E6AC4;
+}
+</style>
