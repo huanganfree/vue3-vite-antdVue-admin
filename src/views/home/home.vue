dc42878a4a99b34fdfd4026ac6d3c8d0a7f93675
huanganfree <huangan1206@163.com>
2025-01-19 12:03:21 +0800

    feat: 完成视频播放截图初步功能

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,28 +1,44 @@
 <template>
   <div class="box">
-    <video controls ref="videoRef"
-      src="https://qt-minio.ictshop.com.cn:9000/resource-management/2025/01/08/a2aa1ec04f244c149a3f8890e2556274.mp4"></video>
-    <img src="@/assets/images/fetch.jpg" alt="">
+    <div class="video-box">
+      <video controls ref="videoRef" crossOrigin='anonymous'
+        src="https://qt-minio.ictshop.com.cn:9000/resource-management/2025/01/08/a2aa1ec04f244c149a3f8890e2556274.mp4"></video>
+      <a-button type="primary" @click="saveCoverImg">保存为封面</a-button>
+    </div>
+
+    <img :src="coverImg" alt="">
   </div>
 </template>
 <script setup>
 import { ref, onMounted } from 'vue';
 
 const videoRef = ref(null)
+const coverImg = ref(null)
 
 onMounted(() => {
 
-  videoRef.value.currentTime = 400
-  videoRef.value.oncanplay = function () {
-    console.log(1221);
-    drawImage(videoRef.value)
-  }
+
 });
 
-function drawImage(video) {
+function saveCoverImg() {
+  drawImage()
+}
+
+function drawImage() {
   const c = document.createElement("canvas");
   const ctx = c.getContext("2d");
-  ctx.drawImage(video, 0, 0);
+  c.width = videoRef.value.videoWidth;
+  c.height = videoRef.value.videoHeight;
+  videoRef.value.currentTime = 4000
+  videoRef.value.onloadeddata = function () {
+    console.log('this', this);
+    ctx.drawImage(videoRef.value, 0, 0, c.width, c.height);
+    const img = c.toDataURL("image/png");
+    console.log(img);
+    coverImg.value = img;
+  }
+  videoRef.value.src = "https://qt-minio.ictshop.com.cn:9000/resource-management/2025/01/08/a2aa1ec04f244c149a3f8890e2556274.mp4";
+  videoRef.value.play();
 }
 
 
@@ -30,13 +46,19 @@ function drawImage(video) {
 
 <style lang="less" scoped>
 .box {
-  video {
-    width: 490px;
+  .video-box {
+    display: flex;
+    align-items: flex-end;
+
+    video {
+      width: 490px;
+      margin: 10px 0 0 0;
+    }
   }
 
   img {
-    width: 490px;
-    height: 300px;
+    width: 300px;
+    margin-top: 10px;
   }
 }
 </style>
